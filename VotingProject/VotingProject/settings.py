c7b12f05caa4ee83c3958f66c3a10b0a9cd654c6
Tarun9905 <ruykjr@gmail.com>
2025-04-17 12:35:20 +0530

    git

--- VotingProject/VotingProject/settings.py
+++ VotingProject/VotingProject/settings.py
@@ -156,12 +156,12 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "your-default-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(" ")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 # Application definition
 INSTALLED_APPS = [
